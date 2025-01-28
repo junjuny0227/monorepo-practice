8630aae761aebca7140045de17a0ffe4452e78bb
junjuny0227 <junjuny0227@gmail.com>
2025-01-28 18:44:05 +0900

    feat: create Test component

---
+++ packages/shared/Test.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const Test = () => {
+  const [bool, setBool] = useState<boolean>();
+
+  console.log(bool);
+
+  return <button onClick={() => setBool((prev) => !prev)}>test</button>;
+};
+
+export default Test;
